3bd40f1fce3c94392c7391e9fe56b70a585f5b6f
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2023-12-23 11:07:15 +0400

    Update main.js

--- main.js
+++ main.js
@@ -12,52 +12,99 @@ mySetTimeout((delay = 3))
   })
   .catch((error) => (`Error:`, error));
 
-// function makeToys() {
-//   return new Promise((resolve, reject) => {
-//     if (Math.random() > 0.1) {
-//       resolve("Undefected");
-//     } else {
-//       reject("Defected");
-//     }
-//   });
-// }
-
-async function makeToys() {
-  if (Math.random() > 0.1) {
-    resolve("Undefected");
-  } else {
-    reject("Defected");
-  }
+function makeToys() {
+  return new Promise((resolve, reject) => {
+    mySetTimeout(3000)
+      .then(() => {
+        console.log("Undefected");
+        resolve();
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
-async function sellToys(status) {
-  if (status === "Undefined") {
-    if (Math.random() > 0.7) {
-      resolve("Toy has been sold");
-    } else {
-      reject("Toy was unseccessful");
-    }
-  }
+function deliverToys() {
+  return new Promise((resolve, reject) => {
+    mySetTimeout(2000)
+      .then(() => {
+        console.log("Toy is successfully delivered");
+        resolve();
+      })
+      .catch(() => {
+        reject("Failed to deliver");
+      });
+  });
 }
-// function sellToys(status) {
-//   return new Promise((resolve, reject) => {
-//     if (status === "Undefined") {
-//       if (Math.random() > 0.7) {
-//         resolve("Toy has been sold");
-//       } else {
-//         reject("Toy was unseccessful");
-//       }
-//     }
-//   });
-// }
-async function promisify() {
+
+function sellToys(status) {
+  return new Promise((resolve, reject) => {
+    mySetTimeout(1000)
+      .then(() => {
+        if (status === "Undefected") {
+          if (Math.random() > 0.5) {
+            resolve();
+            console.log("Toy has been sold");
+          } else {
+            reject("Toy was unsuccessfull");
+          }
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+function promisify() {
+  makeToys()
+    .then(() => deliverToys())
+    .then(() => sellToys("Undefected"))
+    .catch((error) => console.error("Error:", error));
+}
+
+async function makeToys2() {
   try {
-    const status = await makeToys();
-    const result = await sellToys(status);
-    console.log(result);
-  } catch (error) {}
+    await mySetTimeout(3000);
+    console.log("Undefected");
+  } catch (error) {
+    throw error;
+  }
 }
-// makeToys()
-//   .then((status) => sellToys(status))
-//   .then((res) => console.log(res))
-//   .catch((err) => console.log(err));
+async function deliverToys2() {
+  try {
+    await mySetTimeout(2000);
+    console.log("The second toy is successfully delivered");
+  } catch (error) {
+    throw new Error("failed to deliver");
+  }
+}
+
+async function sellToys2(status) {
+  try {
+    await mySetTimeout(1000);
+    if (status === "Undefected") {
+      if (Math.random() > 0.5) {
+        console.log("The second Toy has been sold");
+      } else {
+        console.log("Toy was unsuccessfull");
+      }
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function promisify2() {
+  try {
+    const status = await makeToys2();
+    await deliverToys2();
+    const result = await sellToys2("Undefected");
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+promisify();
+promisify2();
